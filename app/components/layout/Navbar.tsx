ce50ca210a826fc1019dc92fccecc3afd9c5a957
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-17 05:16:39 +0530

    feat(Navbar): add navigation links and make it responsive

--- app/components/layout/Navbar.tsx
+++ app/components/layout/Navbar.tsx
@@ -4,23 +4,29 @@ import { useState } from "react";
 import NavLinks from "../ui/NavLinks";
 import { handleThemeSwitch } from "@/app/utils/handleThemeSwitch";
 import Logo from "../ui/Logo";
+import HamburgerMenu from "./HamburgerMenu";
 
 export default function Navbar() {
   const [isLight, setIsLight] = useState(true);
 
   return (
-    <nav className="bg-[var(--secondary-background)] py-2 px-4 fixed w-full flex flex-nowrap items-center justify-between">
+    <nav className="bg-[var(--secondary-background)] py-2 px-4 fixed max-w-full w-full flex flex-nowrap items-center justify-between z-50">
       <Logo />
+      <div className="hidden sm:block">
       <NavLinks />
-      <div className="hover:cursor-pointer">
+      </div>
+      <div className="flex gap-4">
+      <div className="block sm:hidden">
+        <HamburgerMenu />
+      </div>
         {isLight ? (
           <FaRegLightbulb
-            className="text-white text-[1.7rem]"
+            className="text-white text-[1.7rem] hover:cursor-pointer"
             onClick={() => handleThemeSwitch(setIsLight, isLight)}
           />
         ) : (
           <FaLightbulb
-            className="text-yellow-300 text-[1.7rem]"
+            className="text-yellow-300 text-[1.7rem] hover:cursor-pointer"
             onClick={() => handleThemeSwitch(setIsLight, isLight)}
           />
         )}
