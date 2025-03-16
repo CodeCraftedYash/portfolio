fe8ede54c5e343be94f530e1dff2e6f689383381
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-17 05:25:26 +0530

    feat(navbar): add Hamburger Menu system for navigation on mobile devices

---
+++ app/components/layout/HamburgerMenu.tsx
@@ -0,0 +1,22 @@
+"useClient";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { IoClose } from "react-icons/io5";
+import { useState } from "react";
+import NavLinks from "../ui/NavLinks";
+import HamburgerLinks from "../ui/HamburgerLinks";
+function HamburgerMenu() {
+    const [isClicked,setIsClicked] = useState<boolean> (false);
+  return (
+    <div className="relative">
+    {
+        !isClicked?<GiHamburgerMenu className="text-[var(--secondary-text-color)] text-4xl" onClick={()=>setIsClicked(!isClicked)}/> : <IoClose className="text-[var(--secondary-text-color)] text-4xl" onClick={()=>setIsClicked(!isClicked)}/>
+
+    }
+    {
+        isClicked?<HamburgerLinks />:""
+    }
+    </div>
+  )
+}
+
+export default HamburgerMenu
