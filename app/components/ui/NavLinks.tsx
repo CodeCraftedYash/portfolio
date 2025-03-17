0b7c90474898797e5039c3233f6627a57d5b24f5
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-17 05:30:15 +0530

    feat(navbar): add NavLinks ui component to generate styled list for navbar on non mobile devices

--- app/components/ui/NavLinks.tsx
+++ app/components/ui/NavLinks.tsx
@@ -4,19 +4,25 @@ import { usePathname } from "next/navigation";
 import { RiHomeLine } from "react-icons/ri";
 import { IoIosInformationCircle } from "react-icons/io";
 import { GrProjects } from "react-icons/gr";
-const listClassname:string = "text-[var(--secondary-text-color)] text-[1.3rem] space-x-12"
+import { IoMdContact } from "react-icons/io";
+import { twMerge } from "tailwind-merge";
+import clsx from "clsx";
+const listClassname:string = "text-[var(--secondary-text-color)] text-[1.2rem] space-x-12"
 const navItems = [
     { href: "/", label: "Home",icon:<RiHomeLine className={listClassname} /> },
     { href: "/about", label: "About", icon:<IoIosInformationCircle className={listClassname} /> },
     { href: "/projects", label: "Projects",icon:<GrProjects className={listClassname} />},
+    { href: "/contact", label: "Contact Me",icon:<IoMdContact className={listClassname} />},
   ];
-  
+  const listitemClassname:string =
+    "flex flex-nowrap items-center space-x-2 hover:scale-105 text-[var(--secondary-text-color)]";
+
   export default function NavLinks() {
     const currentPath:string = usePathname();
     return (
-      <ul className="flex space-x-16">
+      <ul className="w-full flex flex-nowrap gap-x-8">
         {navItems.map((item) => (
-          <li key={item.href} className="flex flex-nowrap items-center space-x-2 hover:scale-105">
+          <li key={item.href} className={`${listitemClassname} ${item.href==currentPath ? "border-b-4 border-[var(--background)] text-[var(--primary-text-color)]" : ""}`}>
             {item.icon}
             <Link href={item.href} className={listClassname}>
               {item.label}
