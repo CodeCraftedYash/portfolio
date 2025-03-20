03556b4ce2a63312f7fdcebd05fe3f21200c3431
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-20 10:52:48 +0530

    feat(title): add text and variant prop to the Title component for efficient reusability
    added text and variant prop to title component , through this we can now send the text body itself and variant decides the text sizes , so for now there are three vairants primary , secondary , and tertiary .

--- app/components/ui/Title.tsx
+++ app/components/ui/Title.tsx
@@ -1,15 +1,21 @@
+import clsx from "clsx";
 import { Spicy_Rice } from "next/font/google"
 
 const spicyRice = Spicy_Rice({
     weight: "400",
-    variable: "--font-spicy-rice",
     subsets: ["latin"],
   });
-function Title() {
-  const titleText:string = "Hi I am Yash Mishra" 
+function Title({text,variant}:{text:string,variant:"primary"|"secondary"|"tertiary"}) {
   return (
-    <div className="text-[val(--background)]">
-        <h4 className={`text-white text-5xl ${spicyRice.className} `}style={{ wordSpacing: "0.2em" }}>{titleText}</h4>
+    <div className="text-[var(--background)]">
+        <h4 className={clsx("text-white text-center",
+          spicyRice.className,
+          variant === "primary" ? "text-[2.1rem] sm:text-[3.5rem]" : variant==="secondary"?"text-[1.6rem] sm:text-[2rem]":"text-[.7rem ] sm:text-[1.3rem]"
+        )}
+        style={{
+           wordSpacing: "0.2em",
+          
+         }}>{text}</h4>
     </div>
   )
 }
