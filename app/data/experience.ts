d5700cda5e65eade8442f30346b46356c709766c
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-24 01:55:00 +0530

    feat(experience): add experience data file to store all the experience related information

---
+++ app/data/experience.ts
@@ -0,0 +1,10 @@
+import { ExperienceDataType } from "../types/experienceType";
+
+export const experienceData : ExperienceDataType[] = [
+    {
+        companyName:"Branduck",
+        employedDate:"Oct 2023 - Jan 2024",
+        role:"Intern",
+        description:"I was part of the tech team where I had to write sql queries and use HTML and CSS for making some components",
+    },
+]
