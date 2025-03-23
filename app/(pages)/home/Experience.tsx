06eab8a670335812947743e2ef2c35f7530c0c11
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-23 23:52:57 +0530

    feat(experience): add Experience component to display experience in home page

---
+++ app/(pages)/home/Experience.tsx
@@ -0,0 +1,12 @@
+import ExperienceLayout from "@/app/components/layout/ExperienceLayout"
+import Title from "@/app/components/ui/Title"
+
+function Experience() {
+  return (
+    <div className="w-full">
+        <ExperienceLayout /> 
+    </div>
+  )
+}
+
+export default Experience
