63adbd14d26a6577018a71ee3270ad4983cb9e1d
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-24 16:06:42 +0530

    feat(Experience): adjust code to dynamically add experience card using ExperienceCard UI and experience data

--- app/components/layout/ExperienceLayout.tsx
+++ app/components/layout/ExperienceLayout.tsx
@@ -1,14 +1,23 @@
+import { experienceData } from "@/app/data/experience"
+import ExperienceCard from "../ui/ExperienceCard"
 import Lines from "../ui/Lines"
 import Title from "../ui/Title"
 function ExperienceLayout() {
   return (
       <div className="w-full">
-        <div className="w-full bg-[var(--secondary-background)] flex flex-col items-center">
+        <div className="w-full bg-[var(--secondary-background)] flex flex-col items-center pb-8">
           <div className="my-4">
             <Title text="Experience" variant="primary"/>
           </div>
             <div className="w-8 aspect-square bg-cyan-700 rounded-full"></div>
-            <Lines totalLines={8} />
+            <Lines totalLines={12} />
+            {
+              experienceData.map((item,index)=>(
+                <div key={index}>
+                  <ExperienceCard companyName={item.companyName} employedDate={item.employedDate} role={item.role} description={item.description} />  
+                </div>
+              ))
+            }
         </div>
     </div>
   )
