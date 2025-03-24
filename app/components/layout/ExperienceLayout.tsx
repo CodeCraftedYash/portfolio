23c4e5eff5456a7e5359adb34c5b4453486586ff
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-24 16:25:24 +0530

    refractor(experience): adjust the experience layout to put the newer experince above the previous experience

--- app/components/layout/ExperienceLayout.tsx
+++ app/components/layout/ExperienceLayout.tsx
@@ -10,14 +10,16 @@ function ExperienceLayout() {
             <Title text="Experience" variant="primary"/>
           </div>
             <div className="w-8 aspect-square bg-cyan-700 rounded-full"></div>
-            <Lines totalLines={12} />
+            <div className="flex flex-col-reverse">
             {
               experienceData.map((item,index)=>(
-                <div key={index}>
+                <div key={index} className=" flex flex-col items-center">
+                  <Lines totalLines={12} />
                   <ExperienceCard companyName={item.companyName} employedDate={item.employedDate} role={item.role} description={item.description} />  
                 </div>
               ))
             }
+            </div>
         </div>
     </div>
   )
