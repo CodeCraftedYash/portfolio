4b309fed512635e97ecff45cdb2bed354365cae6
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-24 16:10:40 +0530

    feat(title): adjust code to add micro variant in title ui

--- app/components/ui/Title.tsx
+++ app/components/ui/Title.tsx
@@ -5,12 +5,12 @@ const spicyRice = Spicy_Rice({
     weight: "400",
     subsets: ["latin"],
   });
-function Title({text,variant}:{text:string,variant:"primary"|"secondary"|"tertiary"}) {
+function Title({text,variant}:{text:string,variant:"primary"|"secondary"|"tertiary"|"micro"}) {
   return (
     <div className="text-[var(--background)]">
         <h4 className={clsx("text-white",
           spicyRice.className,
-          variant === "primary" ? "text-[1.7rem] sm:text-[3.5rem]" : variant==="secondary"?"text-[1.2rem] sm:text-[2rem]":"text-[.5rem ] sm:text-[1.3rem]"
+          variant === "primary" ? "text-[1.7rem] sm:text-[3.5rem]" : variant==="secondary"?"text-[1.2rem] sm:text-[2rem]": variant=="tertiary"?"text-[.5rem ] sm:text-[1.3rem]":"text-[.4rem] sm:text-[.8rem]"
         )}
         style={{
            wordSpacing: "0.2em",
