79e668cf0d40bcd956dcb653da812e88f9a36336
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-27 00:03:55 +0530

    feat(type): add types for quick links and contact details

---
+++ app/types/contactAndLinksDataType.ts
@@ -0,0 +1,11 @@
+export type contactDataType = {
+    mobNo:number;
+    email:string;
+    addr:string;
+}
+
+export type quickLinksDataType = {
+    title:string;
+    href:string;
+    logo:React.ReactNode;
+}
