b20e49896cf1fea8e7c97782bee1fcee23ed77a3
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-03-28 00:06:03 +0530

    chore(data): add few icons and quick links of the sites or libraries used in this site

--- app/data/contactAndLinksData.tsx
+++ app/data/contactAndLinksData.tsx
@@ -1,15 +1,20 @@
 import { type contactDataType, type quickLinksDataType } from "../types/contactAndLinksDataType"
 import { RiNextjsFill } from "react-icons/ri";
-import { FaPlaneArrival } from "react-icons/fa";
+import { FaLocationPin } from "react-icons/fa6";
 import { CiMail } from "react-icons/ci";
 import { IoCallOutline } from "react-icons/io5";
+import { RiTailwindCssFill } from "react-icons/ri";
+import { TbBrandFramerMotion } from "react-icons/tb";
+import { RiReactjsFill } from "react-icons/ri";
 
 export const contactData:contactDataType ={mobNo:6299700545,email:"yash1000mishra@gmail.com",addr:"Jamshedpur,Jharkhand,India,831011"}
-export const contactDataLogo ={mobNoLogo:<IoCallOutline />,emailLogo:<CiMail />,addrLogo:<FaPlaneArrival />}
+
+export const contactDataLogo ={mobNoLogo:<IoCallOutline className="text-green-600  rounded-full font-extrabold text-lg"/>,emailLogo:<CiMail className="text-red-700 font-extrabold  text-2xl"/>,addrLogo:<FaLocationPin className="text-xl text-blue-600x"/>}
 
 export const quickLinksData:quickLinksDataType[] = [
-    {title:"NextJs",href:"",logo:<RiNextjsFill />},
-    {title:"TailwindCSS",href:"",logo:<RiNextjsFill />},
-    {title:"Motion",href:"",logo:<RiNextjsFill />},
-    {title:"ReactJs",href:"",logo:<RiNextjsFill />},
+    {title:"NextJs",href:"https://nextjs.org/",logo:<RiNextjsFill className="text-sm "/>},
+    {title:"TailwindCSS",href:"https://tailwindcss.com/",logo:<RiTailwindCssFill className="text-sm text-blue-500"/>},
+    {title:"Motion",href:"https://motion.dev/",logo:<TbBrandFramerMotion className="text-sm text-purple-600"/>},
+    {title:"ReactJs",href:"https://react.dev/",logo:<RiReactjsFill className="text-sm text-blue-500" />},
+    {title:"React-icons",href:"https://react-icons.github.io/react-icons/",logo:<RiReactjsFill className="text-sm text-red-500" />},
 ]
