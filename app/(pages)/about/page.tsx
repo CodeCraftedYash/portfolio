4da518f57fe77cd74f7844a981332c7fc6bed746
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 10:31:49 +0530

    refractor(education): change the background color to bg-slate-600

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -3,11 +3,11 @@ import SelfIntro from "./SelfIntro"
 import Education from "./Education"
 function page() {
   return (
-    <div className="w-full min-h-[calc(100vh-10rem)] pt-16">
+    <div className="w-full min-h-[100dvh] pt-14">
       <div className="p-3 bg-[var(--secondary-background)] mt-2 text-center">
         <Title text="ABOUT ME" variant="primary"/>
       </div>
-      <div className="w-full p-4 min-h-20">
+      <div className="w-full p-4 min-h-20 lg:bg-slate-600">
         <SelfIntro />
       </div>
       <div>
