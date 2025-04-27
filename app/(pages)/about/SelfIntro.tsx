31bc079954d453169ccd766c5f2cdf5b1e71c56a
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 10:30:19 +0530

    refractor(Education): change the font style back to normal

--- app/(pages)/about/SelfIntro.tsx
+++ app/(pages)/about/SelfIntro.tsx
@@ -2,21 +2,21 @@ import Title from "@/app/components/ui/Title"
 import { text1,text2,text3,text4, } from "@/app/data/selfIntroData"
 
 function SelfIntro() {
-    const baseClassName = "text-lg my-2 text-white bg-[var(--secondary-background)] p-4 rounded-2xl  leading-8 md:w-[70%] md:mx-auto underline underline-offset-8 md:hover:scale-105 md:hover:drop-shadow-sm transition-all shadow-red-500 tracking-wider"
+    const baseClassName = "text-lg my-2 text-white bg-[var(--secondary-background)] p-4 rounded-2xl md:text-xl  leading-8 md:w-[70%] md:mx-auto  md:hover:scale-105 md:hover:drop-shadow-sm transition-all shadow-red-500 tracking-wider"
   return (
     <div>
         <div className={`${baseClassName}`}>
-            <Title text={text1} variant="tertiary"/>
+            <p>{text1}</p>
         </div>
         <div className={`${baseClassName}`}>
-            <Title text={text2} variant="tertiary"/>
+            <p>{text2}</p>    
         </div>
         <div className={`${baseClassName}`}>
-            <Title text={text3} variant="tertiary"/>
+            <p>{text3}</p>
         </div>
         <div className={`${baseClassName}`}>
-            <Title text={text4} variant="tertiary"/>
-        </div>
+            <p>{text4}</p>
+         </div>
     </div>
   )
 }
