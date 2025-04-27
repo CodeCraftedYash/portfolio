b278b9ddcec0cd0e7eb179c10b7844ef25c1cdcb
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 12:09:23 +0530

    feat(data): add data file for Education component to generate education details based on this data

---
+++ app/data/EducationData.tsx
@@ -0,0 +1,5 @@
+import { type EducationDataType } from "../types/educationType";
+
+export const EducationData:EducationDataType[] = [
+    {courseName:"Bachelor of Science in Information Technology", institutionName:"Kolhan University", date:"2023-2026",location:"Jharkhand, India"}
+]
