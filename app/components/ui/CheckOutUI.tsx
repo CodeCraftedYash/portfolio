600732f5a0be10aa6d4edaa3efb42cfe94bec763
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 11:06:17 +0530

    refractor(ui): adjust the code for better readability

--- app/components/ui/CheckOutUI.tsx
+++ app/components/ui/CheckOutUI.tsx
@@ -9,10 +9,10 @@ function CheckOutUI({title,href}:CheckoutType) {
 
   return (
     <Link href={href} prefetch>
-    <li className="w-24 sm:w-32  aspect-square flex items-center justify-center rounded-2xl border-2 border-white mx-2 my-2 gap-2 sm:hover:cursor-pointer sm:hover:scale-105 transition-all sm:hover:bg-amber-800/50 px-2 sm:justify-start text-center bg-[var(--secondary-background)]">
+    <div className="w-24 sm:w-32  aspect-square flex items-center justify-center rounded-2xl border-2 border-white mx-2 my-2 gap-2 sm:hover:cursor-pointer sm:hover:scale-105 transition-all sm:hover:bg-amber-800/50 px-2 sm:justify-start text-center bg-[var(--secondary-background)]">
         <Title text={title} variant="tertiary" />
         <HiArrowTopRightOnSquare className="text-[var(--background)] sm:text-2xl"/>
-    </li>
+    </div>
     </Link>
   )
 }
