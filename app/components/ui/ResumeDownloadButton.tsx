1db16da996dbcb5051966170d8f40dd1621ecaf3
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 12:07:50 +0530

    feat(ResumeDownloadButton): add loading animation for pending status

--- app/components/ui/ResumeDownloadButton.tsx
+++ app/components/ui/ResumeDownloadButton.tsx
@@ -3,12 +3,14 @@ import { IoMdDownload } from "react-icons/io";
 import { useResumeDownload } from '@/app/hooks/useResumeDownload';
 import { startTransition } from 'react';
 import Button from './Buttons';
+import Loading from "../animation/Loading";
 export default function ResumeDownloadButton() {
   const [error, handleDownload, isPending] = useResumeDownload();
 
   return (
+     isPending ? <div className=" bg-white p-4 rounded-2xl"><Loading text="Downloading"/></div> :
     <Button variant="primary" onClick={() => startTransition(() => handleDownload())}>
-      {isPending ? 'Downloading...' : <h4 className='flex items-center flex-nowrap gap-x-2'>My Resume <span><IoMdDownload /></span></h4>}
+      <h4 className='flex items-center flex-nowrap gap-x-2'>My Resume <span><IoMdDownload /></span></h4>
     </Button>
   );
 }
