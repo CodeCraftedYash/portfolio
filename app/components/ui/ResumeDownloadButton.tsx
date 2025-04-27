46c3a6507073514da0a42ba149a5e70ec3c587a1
CodeCraftedYash <yashkumarmishra1000@gmail.com>
2025-04-27 14:41:12 +0530

    fix(ui): add code to display error message in resume download button component

--- app/components/ui/ResumeDownloadButton.tsx
+++ app/components/ui/ResumeDownloadButton.tsx
@@ -8,9 +8,16 @@ export default function ResumeDownloadButton() {
   const [error, handleDownload, isPending] = useResumeDownload();
 
   return (
+    <>
      isPending ? <div className=" bg-white p-4 rounded-2xl"><Loading text="Downloading"/></div> :
     <Button variant="primary" onClick={() => startTransition(() => handleDownload())}>
       <h4 className='flex items-center flex-nowrap gap-x-2'>My Resume <span><IoMdDownload /></span></h4>
     </Button>
+    {error && (
+        <p className="text-red-500 mt-2 text-sm">
+          {error}
+        </p>
+      )}
+    </>
   );
 }
